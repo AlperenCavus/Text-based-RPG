26b971ff7d28e0f92cc87a036e075cdf606b08f5
AlperenCavus <116302486+AlperenCavus@users.noreply.github.com>
2023-01-11 15:49:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -28,25 +28,12 @@ public class Main {
 
 			}
 		}
-		choice();
+		
 		
 
 
 
 	}
-	public static void choice() {
-		Scanner scan= new Scanner(System.in);
-		int choice;
-		System.out.println("1-Yes 2-No");
-		do {
-		  choice=scan.nextInt();
-		  if(choice!=1 || choice==2)
-			  continue;
-		 }
-		 while(choice==1 || choice!=2);
-		if(choice==2) {
-			System.out.println("See you soon then!");
-		}
-	}
+	
 		
 }
